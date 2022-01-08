ce80b5b59d1cbd146ab6393e43ab6e1e941bf7c4
Ieonard04 <leonardogilrojo@gmail.com>
2022-01-07 20:26:29 -0800

    Initial Commmit

---
+++ src/main/java/me/Ieonerd/simplehud/SimpleHUD.java
@@ -0,0 +1,15 @@
+package me.Ieonerd.simplehud;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SimpleHUD implements ClientModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("simplehud");
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/me/Ieonerd/simplehud/mixin/MixinInGameHud.java
@@ -0,0 +1,34 @@
+package me.Ieonerd.simplehud.mixin;
+
+import me.Ieonerd.simplehud.gui.SimpleHUD;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Environment(EnvType.CLIENT)
+@Mixin(InGameHud.class)
+public class MixinInGameHud extends DrawableHelper {
+
+    @Shadow @Final private MinecraftClient client;
+    SimpleHUD simpleHUD;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    public void addSimpleHudToHUD(MinecraftClient client, CallbackInfo ci){
+        simpleHUD = new SimpleHUD(client);
+    }
+
+    @Inject(method = "render", at = @At("TAIL"))
+    public void renderSimpleHud(MatrixStack matrices, float tickDelta, CallbackInfo ci){
+        if(!this.client.options.debugEnabled) simpleHUD.render(matrices);
+    }
+
+}
